146dd3a8f03ab06f1b6714602c5a65e728bf5a9b
Iesus Magdiel <76500125+iesusdeveloper@users.noreply.github.com>
2021-09-20 14:58:57 -0300

    Add files via upload

---
+++ calculo_gotas.py
@@ -0,0 +1,11 @@
+# ESTE SISTEMA É UTILIZADO POR EQUIPE MÉDICA E PRINCIPALMENTE PELA A EQUIPE DE ENFERMAGEM PARA A ADMINISTRAÇÃO DE MEDICAÇÕES LÍQUIDAS POR UM DERTERMINADO TEMPO E COM UMA DETERMINADA VELOCIDADE, SENDO MEDIDO PELA QUANTIDADE DE GOTAS POR MINUTO.
+tipo = str(input(
+    'Digite o nome do cálculo, se é MACROGOTAS ou MICROGOTAS: ')).strip().upper()
+volume = int(input('Quantos ml de líquido será infudido? '))
+tempo = float(input('Em quantas horas foi prescrito? '))
+calculo_macro = volume / (tempo * 3)
+calculo_micro = volume / tempo
+if tipo == 'MACROGOTAS' or tipo == 'MACROGOTA':
+    print('O resultado é {} macrogotas/minuto.'.format(round(calculo_macro)))
+if tipo == 'MICROGOTAS' or tipo == 'MICROGOTA':
+    print('O resultado é {} microgotas/minuto.'.format(round(calculo_micro)))
